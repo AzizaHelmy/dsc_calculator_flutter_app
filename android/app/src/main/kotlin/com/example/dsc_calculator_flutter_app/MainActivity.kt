3516047d86fb972ee277aab1fa2054bc07c227e9
azizaHelmy <aziza.helmy35@gmail.com>
2020-11-30 21:19:05 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/dsc_calculator_flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dsc_calculator_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange,
+          title: Text("Calculator App"),
+        ),
+        body: Column(children: [],),
+      ),
+    );
+  }
+}
